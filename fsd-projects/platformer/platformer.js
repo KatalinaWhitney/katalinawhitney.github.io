c9f3df8c63f733dbe6121a2d2ea7376aa92746e7
KatalinaWhitney <katalina.whitney@jpschools.org>
2026-09-01 15:54:51 +0000

    Final Game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(248, 183, 183)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(248, 183, 183)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -27,34 +33,32 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-     // toggleGrid();
-
+    // toggleGrid();
 
     // TODO 2 - Create Platforms
-createPlatform(400, 0, 20, 290, "black");
-createPlatform(200, 700, 60, 10, "white"); 
-createPlatform(400, 600, 60, 10, "white");
-createPlatform(650, 500, 60, 10, "white");
-createPlatform(920, 410, 60, 10, "white");
-createPlatform(650, 310, 60, 10, "white");
-createPlatform(920, 220, 60, 10, "white");
-createPlatform(1160, 130, 60, 10, "white");
+    createPlatform(400, 0, 20, 290, "black");
+    createPlatform(200, 700, 60, 10, "white");
+    createPlatform(400, 600, 60, 10, "white");
+    createPlatform(650, 500, 60, 10, "white");
+    createPlatform(920, 410, 60, 10, "white");
+    createPlatform(650, 310, 60, 10, "white");
+    createPlatform(920, 220, 60, 10, "white");
+    createPlatform(1160, 130, 60, 10, "white");
 
     // TODO 3 - Create Collectables
-createCollectable("database", 400, 565, 0.5, 0);
-createCollectable("database", 200, 175, 0.5, 0);
-createCollectable("database", 655, 465, 0.5, 0);
-createCollectable("database", 930, 375, 0.5, 0);
-createCollectable("database", 660, 275, 0.5, 0);
-createCollectable("database", 930, 175, 0.5, 0);
-createCollectable("database", 1170, 75, 0.5, 0)
-    
-    // TODO 4 - Create Cannons
-createCannon("top", 370, 1000)
-createCannon("top", 590, 950)
-createCannon("top", 850, 990)
+    createCollectable("database", 400, 565, 0.5, 0);
+    createCollectable("database", 200, 175, 0.5, 0);
+    createCollectable("database", 655, 465, 0.5, 0);
+    createCollectable("database", 930, 375, 0.5, 0);
+    createCollectable("database", 660, 275, 0.5, 0);
+    createCollectable("database", 930, 175, 0.5, 0);
+    createCollectable("database", 1170, 75, 0.5, 0);
 
-    
+    // TODO 4 - Create Cannons
+    createCannon("top", 370, 1000);
+    createCannon("top", 590, 950);
+    createCannon("top", 850, 990);
+
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
