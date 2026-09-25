9e8fa003dec6ac212af15c68068424492d47cda9
KatalinaWhitney <katalina.whitney@jpschools.org>
2026-09-25 14:48:01 +0000

    completed runtime-neo project

--- fsd-projects/runtime/scenery.js
+++ fsd-projects/runtime/scenery.js
@@ -14,11 +14,25 @@ const scenery = {
   building: {
     imageUrl: "images/backgrounds/building.png",
     loopWidth: 1400,
-    instances: [{ x: 400, width: 100, height: 300, speedX: -2 }],
+    instances: [
+      { x: 0, width: 120, height: 300, speedX: -2 },
+      { x: 240, width: 130, height: 330, speedX: -2 },
+      { x: 500, width: 110, height: 280, speedX: -2 },
+      { x: 760, width: 140, height: 320, speedX: -2 },
+      { x: 1040, width: 120, height: 300, speedX: -2 },
+      { x: 1320, width: 150, height: 340, speedX: -2 },
+    ],
   },
   lamp: {
     imageUrl: "images/backgrounds/lamp.png",
     loopWidth: 1400,
-    instances: [{ x: 700, width: 50, height: 150, speedX: 0 }],
+    instances: [
+      { x: 200, width: 50, height: 150, speedX: 0 },
+      { x: 450, width: 50, height: 150, speedX: 0 },
+      { x: 700, width: 50, height: 150, speedX: 0 },
+      { x: 950, width: 50, height: 150, speedX: 0 },
+      { x: 1200, width: 50, height: 150, speedX: 0 },
+      { x: 1450, width: 50, height: 150, speedX: 0 },
+    ],
   },
 };
